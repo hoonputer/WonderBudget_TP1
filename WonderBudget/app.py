b40367ed401e08abd027ea257fadb5163b498e57
Lee Hyunju <guswnee00@gmail.com>
2023-07-26 14:39:20 +0900

    app.py 모델불러오기 추가

--- WonderBudget/app.py
+++ WonderBudget/app.py
@@ -6,7 +6,9 @@ import joblib
 app = Flask(__name__)
 
 # 모델 불러오기
-
+h_model = joblib.load(open('WonderBudget/data/pkl/hotel_model.pkl', 'rb'))
+f_model = joblib.load(open('WonderBudget/data/pkl/flight_model.pkl', 'rb'))
+c_model = joblib.load(open('WonderBudget/data/pkl/car_model.pkl', 'rb'))
 
 # 에러페이지
 @app.errorhandler(404)
@@ -14,33 +16,43 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 # 메인 페이지
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.heml')
-
-# 예산이 정해져 있다면?
-@app.route('/yes_budget', methods = ['GET', 'POST'])
-def yes_budget():
-    # '예산이 정해져 있다'를 선택했을 때 보이는 창
+    # 처음 화면
     if request.method == 'GET':
-        return render_template('-.html')
+        return render_template('index.heml')
     
-    # 단순 추천 시스템을 활용해 추천 호텔, 항공권, 렌터카 보여주기
-    if request.methodn == 'POST':
+    # 값 입력
+    if request.method == 'POST':
+        try:
+            # 호텔 가격 예측
+            h_data1 = float(request.form['h_data1'])
+            h_data2 = float(request.form['h_data2'])
+            h_data3 = float(request.form['h_data3'])
+            h_array = np.array([[h_data1, h_data2, h_data3]])
+            h_pred = int(h_model.predict(h_array))
 
-        return render_template('-.html')
+            # 항공권 가격 예측
+            f_data1 = float(request.form['f_data1'])
+            f_data2 = float(request.form['f_data2'])
+            f_data3 = float(request.form['f_data3'])
+            f_array = np.array([[f_data1, f_data2, f_data3]])
+            f_pred = int(h_model.predict(f_array))
 
-# 예산이 정해져 있지 않다면?
-@app.route('/no_budget', methods = ['GET', 'POST'])
-def no_budget():
-    # '정해진 예산이 없다'를 선택했을 때 보이는 창
-    if request.method == 'GET':
-        return render_template('-.html')
+            # 렌트카 가격 예측
+            c_data1 = float(request.form['c_data1'])
+            c_data2 = float(request.form['c_data2'])
+            c_data3 = float(request.form['c_data3'])
+            c_array = np.array([[c_data1, c_data2, c_data3]])
+            c_pred = int(h_model.predict(c_array))
 
-    # 머신러닝을 활용하여 대략적인 총 비용 보여주기
-    if request.methodn == 'POST':
+            # 세 예측값 더하기
+            pred = h_pred + f_pred + c_pred
 
-        return render_template('-.html')
+            return render_template('index.html', pred = pred)
+        
+        except:
+            return render_template('return.index')
 
 if __name__ == "__main__":
     app.run(debug=True)
