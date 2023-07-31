b2657eb7be4cba7d0dac476f812fe09e5bfdca7c
Parksangyoung89 <pmskye89@google.com>
2023-07-31 13:59:05 +0900

    py파일 추가

---
+++ WonderBudget/data/ML/car_model.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import joblib
+
+# Load the dataset
+data = pd.read_csv('car.csv')
+
+# Drop unnecessary columns
+data.drop(columns=['week', 'car_name'], inplace=True)
+
+# Define the target variable and separate features and target
+target = 'price'
+X = data.drop(target, axis=1)
+y = data.loc[:, target]
+
+# Encode categorical features
+encoder = OneHotEncoder(handle_unknown = 'ignore')
+X_encoded = encoder.fit_transform(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.3, random_state=20)
+
+# Initialize the regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# 성능 평가
+print('MAE:', mean_absolute_error(y_test, y_pred))
+print('MSE:', mean_squared_error(y_test, y_pred))
+print('RMSE:', mean_squared_error(y_test, y_pred, squared=False))
+print('R2-Score:', r2_score(y_test, y_pred))
+
+#피클링 파일 생성
+joblib.dump(model, './car_model.pkl')
+
+joblib.dump(encoder, './car_encoder.pkl')
